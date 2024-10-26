bc8de6f3e926ba78ce8d9df36013b1b4bdf252b3
lopogamer <232036958@aluno.unb.br>
2024-10-26 11:05:53 -0300

    adicionando a classe nome e avaliaçao

--- Dominios.h
+++ Dominios.h
@@ -1,45 +1,87 @@
 #ifndef DOMINIOS_H_INCLUDED
 #define DOMINIOS_H_INCLUDED
+
 #include <string>
 using namespace std;
-class Senha{
-    private:
-        string senha;
-        bool validar(string);
-    public:
-        bool SetSenha(string);
-        string GetSenha() const;
+
+class Senha
+{
+private:
+    string senha;
+    bool validar(string);
+
+public:
+    bool SetSenha(string);
+    string GetSenha() const;
 };
 
-inline string Senha::GetSenha() const {
+inline string Senha::GetSenha() const
+{
     return senha;
 }
 
-class Horario{
-    private:
-        string hora;
-        bool Validar(string);
-    public:
-        bool SetHora(string);
-        string GetHora() const;
+class Horario
+{
+private:
+    string hora;
+    bool Validar(string);
+
+public:
+    bool SetHora(string);
+    string GetHora() const;
 };
-inline string Horario::GetHora() const{
+
+inline string Horario::GetHora() const
+{
     return hora;
 }
 
+class Dinheiro
+{
+private:
+    string quantidade;
+    bool Validar(const string&);
 
-class Dinheiro{
-    private:
-        string  quantidade;
-        bool Validar(const string&);
-    public:
-        bool SetDinheiro(string);
-        string GetDinheiro() const;
+public:
+    bool SetDinheiro(string);
+    string GetDinheiro() const;
 };
-inline string Dinheiro::GetDinheiro() const{
-        return quantidade;
+
+inline string Dinheiro::GetDinheiro() const
+{
+    return quantidade;
 }
 
+class Avaliacao
+{
+private:
+    int digito_;
+    bool Validar(int valor) const;
 
+public:
+    bool setValor(int valor);
+    int getValor() const;
+};
+
+inline int Avaliacao::getValor() const
+{
+    return digito_;
+}
+
+class Nome
+{
+private:
+    string nome_;
+    bool Validar(string nome);
+
+public:
+    bool setNome(string nome);
+    string getNome();
+};
+
+inline string Nome::getNome()
+{
+    return nome_;
+}
 
 #endif // DOMINIOS_H_INCLUDED
