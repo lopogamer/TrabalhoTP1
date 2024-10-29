7942d7401baa201b70931e3033995986c92d6b82
lopogamer <232036958@aluno.unb.br>
2024-10-29 10:25:37 -0300

    A

---
+++ include/Entidade.h
@@ -0,0 +1,11 @@
+#ifndef ENTIDADE_H_INCLUDED
+#define ENTIDADE_H_INCLUDED
+
+class Atividade{
+private:
+    Codigo
+
+
+};
+
+#endif // ENTIDADE_H_INCLUDED
--- src/Entidade.cpp
+++ src/Entidade.cpp
@@ -1,4 +1,5 @@
 #include "Entidade.h"
+using namespace std;
 
 int main(){
 
