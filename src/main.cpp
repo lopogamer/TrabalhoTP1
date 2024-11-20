ae361dfff2b1b4b34cdc1f659240d026a384bfe2
João Gabriel MF <232003607@aluno.unb.br>
2024-11-19 21:10:58 -0300

    Exceções da classe Data

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,6 @@ int main()
 
     setlocale(LC_ALL, "portuguese");
 
-    Data data1;
-    Data data2;
-    Data data3;
-
-    string valida = "29-02-16";
-    string invalida = "01-01-100";
-
-    string invalida2 = "29-02-21";
-
-    data1.SetData(valida);
-    cout << data1.GetData() << endl;
-    data2.SetData(invalida);
-    cout << data2.GetData() << endl;
-    data3.SetData(invalida2);
-    cout << data3.GetData() << endl;
-
 
     return 0;
 }
