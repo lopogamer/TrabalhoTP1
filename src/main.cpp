fe47cee940add1014973443c70a1631a657a2e6b
zekekk <89425552+zekekk@users.noreply.github.com>
2024-11-26 10:21:45 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,20 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "portuguese");
+    TUavaliacao teste;
+    switch(teste.run()){
+        case TUavaliacao::SUCESSO: cout << "SUCESSO  - AVALICAO" << endl;
+            break;
+        case TUavaliacao::FALHA:   cout << "FALHA   -  AVALIACAO" << endl;
+            break;
+    }
+
+    TUcodigo teste2;
+    switch(teste2.run()){
+        case TUcodigo::SUCESSO: cout << "SUCESSO  - CODIGO" << endl;
+            break;
+        case TUcodigo::FALHA:   cout << "FALHA   -  CODIGO" << endl;
+            break;
+    }
     return 0;
 }
